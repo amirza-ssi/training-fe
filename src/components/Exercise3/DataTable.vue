15afdaecc2958653ea7751050ae4353e67da53fe
amirza_ssi <amirza@ssidecisions.com>
2024-10-01 19:16:10 +0500

    exercise3 - render data in table

---
+++ src/components/Exercise3/DataTable.vue
@@ -0,0 +1,60 @@
+<template>
+  <tr class="text-left">
+    <th class="p-3 font-extrabold text-green-600" v-for="col in formatColName" :key="col">
+      {{ col.val }}
+    </th>
+  </tr>
+
+  <tr v-for="record in data" :key="record.id">
+    <td v-for="(col, i) in cols" :key="i">{{ col != "image" ? record[col] : "<img src='"+ record[col] +"' / >" }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    data: {
+      type: Array
+    },
+    cols: {
+      type: Array
+    }
+  },
+  // lifecycle hooks of events as given here https://v3.vuejs.org/api/options-lifecycle-hooks.html
+  mounted() {},
+
+  created() {},
+  // end lifecycle hooks
+  components: {},
+  data() {
+    return {
+      rows: []
+    }
+  },
+  computed: {
+    formatColName() {
+      //   console.log(this.cols.values().map((colName) => colName.upper()))
+      return this.cols.map((c) => {
+        return {
+          // capitalize
+          key: c,
+          val: c
+            .split('_')
+            .map((c) => c.charAt(0).toUpperCase() + c.slice(1))
+            .reduce((str, c) => str + ' ' + c, '')
+        }
+      })
+    }
+  },
+  methods: {
+    mapColumnKeysFromData() {
+      var colSet = new Set([])
+      this.data.forEach((row) => (colSet = new Set([...colSet, ...new Set(Object.keys(row))])))
+      //   this.cols = colSet
+
+      console.log(this.cols)
+    }
+  }
+}
+</script>
