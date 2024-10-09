520d7268d584321ab49732892478e42d489ec310
amirza_ssi <amirza@ssidecisions.com>
2024-10-09 19:19:03 +0500

    parent components converted to composition api

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,63 +3,46 @@
     <div class="block h-8 w-40 rounded overflow-hidden bg-green-500 text-center m-6">
       <button @click="handleAddUserClick">Add User</button>
     </div>
-    <DataTable :data="this.userData" :cols="this.dataColumns"></DataTable>
+    <DataTable :data="userData" :cols="dataColumns"></DataTable>
   </div>
   <ModalComponent ref="modalComponent" @confirm="addUserDetails"></ModalComponent>
 </template>
 
-<script>
+<script setup>
 import ModalComponent from '@/components/Exercise2/ModalComponent.vue'
 import DataTable from '@/components/Exercise3/DataTable.vue'
-export default {
-  name: 'AboutView',
-  props: {},
-  // lifecycle hooks of events as given here https://v3.vuejs.org/api/options-lifecycle-hooks.html
-  mounted() {},
-  // end lifecycle hooks
-  components: { ModalComponent, DataTable },
-  data() {
-    return {
-      userData: [],
-      dataColumns: ['name', 'email', 'phone', 'address', 'country']
-    }
-  },
-  computed: {},
-  methods: {
-    handleAddUserClick() {
-      let formConfig = [
-        { input: 'text', label: 'Name', value: '' },
-        { input: 'text', label: 'Email', value: '' },
-        { input: 'text', label: 'Phone', value: '' },
-        { input: 'text', label: 'Address', value: '' },
-        {
-          input: 'dropdown',
-          label: 'Country',
-          value: '',
-          options: [
-            { id: 'PK', title: 'Pakistan' },
-            { id: 'US', title: 'United States' },
-            { id: 'UK', title: 'United Kingdom' },
-            { id: 'CA', title: 'Canada' }
-          ]
-        }
+import { ref, useTemplateRef } from 'vue'
+
+var userData = ref([])
+var dataColumns = ref(['name', 'email', 'phone', 'address', 'country'])
+const modalComponent = useTemplateRef('modalComponent')
+
+function handleAddUserClick() {
+  let formConfig = [
+    { input: 'text', label: 'Name', value: '' },
+    { input: 'text', label: 'Email', value: '' },
+    { input: 'text', label: 'Phone', value: '' },
+    { input: 'text', label: 'Address', value: '' },
+    {
+      input: 'dropdown',
+      label: 'Country',
+      value: '',
+      options: [
+        { id: 'PK', title: 'Pakistan' },
+        { id: 'US', title: 'United States' },
+        { id: 'UK', title: 'United Kingdom' },
+        { id: 'CA', title: 'Canada' }
       ]
-      this.$refs.modalComponent.open('Add User', formConfig)
-
-      // <input
-      //         class="block my-2 border-2 p-2 rounded-lg min-w-40"
-      //         v-model="formData.name"
-      //         placeholder="Name"
-      //       />
-    },
-    addUserDetails(user) {
-      let userObj = {}
-
-      user.forEach((input) => {
-        userObj[input.label.toLowerCase()] = input.value
-      })
-      this.userData.push(userObj)
     }
-  }
+  ]
+  modalComponent.value.open('Add User', formConfig)
+}
+function addUserDetails(user) {
+  let userObj = {}
+
+  user.forEach((input) => {
+    userObj[input.label.toLowerCase()] = input.value
+  })
+  userData.value.push(userObj)
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,24 @@
 <template>
   <DropdownComponent
-    :title="this.dropdownTitle"
-    :options="this.dropdownOptions"
-    confirm="getDropdownInput"
+    :title="dropdownTitle"
+    :options="options"
+    @confirm="getDropdownInput"
   ></DropdownComponent>
 </template>
-<script>
+<script setup>
+import { ref } from 'vue'
 import DropdownComponent from '../components/DropdownComponent.vue'
 
-export default {
-  name: 'HomeView',
-  props: {},
-  mounted() {},
-  components: { DropdownComponent },
-  data() {
-    const options = [
-      { id: 1, title: 'Ruby on Rails' },
-      { id: 2, title: 'Vue.js' },
-      { id: 3, title: 'Tailwind CSS' },
-      { id: 4, title: 'Node JS' }
-    ]
+const options = [
+  { id: 1, title: 'Ruby on Rails' },
+  { id: 2, title: 'Vue.js' },
+  { id: 3, title: 'Tailwind CSS' },
+  { id: 4, title: 'Node JS' }
+]
 
-    return {
-      dropdownTitle: 'Dropdown Title Test',
-      dropdownOptions: options
-    }
-  },
-  computed: {},
-  methods: {}
+function getDropdownInput(r) {
+  console.log(r)
+  dropdownTitle.value = r
 }
+var dropdownTitle = ref('Dropdown Title Test')
 </script>
